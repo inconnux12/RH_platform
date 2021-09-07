601e0c5506e43c0ca63607c1b1096703f27ccb03
inconnux12 <simonnassim9@gmail.com>
2021-09-07 15:46:12 +0100

    added turbolinks

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,7 @@
     </footer>
     <script src="{{asset('js/alp.js')}}"></script>
     @livewireScripts
-    <script src="https://cdn.jsdelivr.net/gh/livewire/turbolinks@v0.1.x/dist/livewire-turbolinks.js" data-turbolinks-eval="false" data-turbo-eval="false"></script>
+    <script src="{{asset('js/livewire-turbolinks.js')}}" data-turbolinks-eval="false" data-turbo-eval="false"></script>
 </body>
     
 </html>
